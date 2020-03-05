394cfaf00a01e11bd4970462f679d11db7fc8776
MaevaSimao <mae.simao6473@gmail.com>
2020-03-05 09:01:14 +0100

    ajout texte a animation

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,7 @@
 
 import '../css/homepage.scss';
+import { red } from 'ansi-colors';
+import { AST_DefaultAssign } from 'terser';
 
 var homepage = {
 
@@ -31,9 +33,32 @@ var homepage = {
 
         // < 992 px
         else {
-            
-        $('#button-animation').before('<p>Paragraphe précédent</p>');
         
+        let $styleBlock = {
+            width: '100%', 
+            'text-align': 'end', 
+            'padding-right': 0, 
+            
+        }
+
+        let $styleP = {
+            'font-size' : '0.6em', 
+        }
+
+        let $blockDescription = $("<div></div>").css($styleBlock); 
+
+        let $description = $("<p>Cliquez ici pour afficher le menu</p>").css($styleP);
+
+        let $linkStop = $("<button> >> passez </button>"); 
+
+   
+
+        $('#button-animation').after( $blockDescription );
+
+        $blockDescription.append($description); 
+
+        $description.after($linkStop); 
+
         homepage.flashBreakSmall(); 
          
             
@@ -43,13 +68,13 @@ var homepage = {
     },
 
     
-    
+    /**
+     * click() btn "parcourir les élements" (< 992px ) 
+     */
     flashBreakSmall:function() {
      
 
-       
-
-        $( '#button-animation' ).fadeOut(700).delay(300).fadeIn(200); 
+        $( '#button-animation' ).fadeOut(700).delay(400).fadeIn(200); 
 
         setInterval(this.flashBreakSmall, 1000)
 
