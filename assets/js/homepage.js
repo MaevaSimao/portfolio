1a0894a867b67f988678157ce27485b9757200e3
MaevaSimao <mae.simao6473@gmail.com>
2020-03-08 18:29:49 +0100

    changement animation

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,12 +1,15 @@
 
 import '../css/homepage.scss';
-import { red } from 'ansi-colors';
-import { AST_DefaultAssign } from 'terser';
+
 
 var homepage = {
 
+    // property for matchMedia min-width:227px
+    breakSmall: window.matchMedia("(min-width: 227px)"),
+
     // property for matchMedia min-width:992px
     breakLarge: window.matchMedia("(min-width: 992px)"),
+
 
     /**
      * init homepage
@@ -26,16 +29,61 @@ var homepage = {
      */
     travel:function() {
 
-        // >= 992px
+        // >= 227px 
+        if (homepage.breakSmall.matches) { 
+            let $styleBlock = {
+                width: '100%', 
+                'text-align': 'end', 
+                'padding-right': 0, 
+                
+            }
+    
+            let $styleP = {
+                'font-size' : '0.6em', 
+            }
+    
+            let $blockDescription = $("<div></div>").css($styleBlock); 
+    
+            let $description = $("<p>Cliquez ici pour afficher le menu</p>").css($styleP);
+    
+            let $linkStop = $("<button> >> passez </button>"); 
+    
+            $('#button-animation').after( $blockDescription );
+    
+            $blockDescription.append($description); 
+    
+            $description.after($linkStop); 
+    
+            homepage.flashBreakSmall(); 
+            console.log('match small'); 
+        } 
+
+
+        // >= 992px 
         if (homepage.breakLarge.matches) { 
             console.log('match large'); 
         } 
 
-        // < 992 px
+        // < 227px 
         else {
+
+            homepage.flashBreakVerySmall(); 
+        
+            console.log('match very small'); 
+            
+        }
+
+        console.log('travel')
+    },
+
+    /**
+     * click() btn "parcourir les élements" (< 227px ) 
+     */
+    flashBreakVerySmall:function() {
+
         
         let $styleBlock = {
-            width: '100%', 
+            width: '50%', 
             'text-align': 'end', 
             'padding-right': 0, 
             
@@ -51,22 +99,19 @@ var homepage = {
 
         let $linkStop = $("<button> >> passez </button>"); 
 
-   
-
-        $('#button-animation').after( $blockDescription );
+        $description.after($linkStop);
 
         $blockDescription.append($description); 
+        $( '#button-animation' ).after($blockDescription); 
 
-        $description.after($linkStop); 
+    
 
-        homepage.flashBreakSmall(); 
-         
-            
-        }
+        setInterval($blockDescription.fadeOut(700).delay(400).fadeIn(200), 1000);
 
-        console.log('travel')
-    },
-
+       
+        
+    }, 
+    
     
     /**
      * click() btn "parcourir les élements" (< 992px ) 
@@ -79,17 +124,7 @@ var homepage = {
         setInterval(this.flashBreakSmall, 1000)
 
         
-     }
-    
-
-   
-
-
-       
-
-   
-    
-   
+    } 
     
 
 }
